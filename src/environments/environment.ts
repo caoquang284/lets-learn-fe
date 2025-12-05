a7b476699768761385eed7924ea9e2e84695384e
caoquang284 <rinquanq05@gmail.com>
2025-12-05 14:42:26 +0700

    Setup for Netlify deployment

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  BACKEND_URL: 'https://basic-result--cedar.trycloudflare.com',
+  // LiveKit feature flags (production)
+  LIVEKIT_ENABLED: true,
+  // LiveKit server websocket URL
+  LIVEKIT_WS_URL: 'ws://45.128.222.24:7880',
+  LIVEKIT_DEFAULT_ROOM: 'test-room',
+  TINYMCE_API_KEY: '',
+  CLOUDINARY_CLOUD_NAME: '',
+  UPLOAD_PRESET_NAME: '',
+  CLOUDINARY_API_KEY: '',
+  CLOUDINARY_API_SECRET: '',
+};
