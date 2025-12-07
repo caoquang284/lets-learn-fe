06585467d7501eba549639aa1ae607c4b7f9aa60
caoquang284 <rinquanq05@gmail.com>
2025-12-07 20:12:53 +0700

    refactor: change URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,14 @@ export const environment = {
   // LiveKit feature flags (production)
   LIVEKIT_ENABLED: true,
   // LiveKit server websocket URL
-  LIVEKIT_WS_URL: 'ws://45.128.222.24:7880',
+  LIVEKIT_WS_URL: 'wss://update-zero-military-founded.trycloudflare.com',
   LIVEKIT_DEFAULT_ROOM: 'test-room',
   TINYMCE_API_KEY: '',
   CLOUDINARY_CLOUD_NAME: '',
   UPLOAD_PRESET_NAME: '',
   CLOUDINARY_API_KEY: '',
   CLOUDINARY_API_SECRET: '',
+  CLOUD_API_KEY: 965853596633558,
+  CLOUD_API_SECRET: 'g7d33_DwBb3DkEXas09fMIarWho',
+  CLOUD_NAME: 'drusgyddb'
 };
